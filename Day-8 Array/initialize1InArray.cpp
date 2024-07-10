18d63b54cc6fac0c763ce668f57ee1c026250f72
GaganKumar-26 <146158851+GaganKumar-26@users.noreply.github.com>
2024-07-10 21:34:14 +0530

    Add files via upload

---
+++ Day-8 Array/initialize1InArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num[10]={ };
+    //printing the array
+    for(int i = 0; i < 10; i++){
+        num[i] = 1;
+        cout<<num[i]<<" ";
+    }
+    return 0;
+}
